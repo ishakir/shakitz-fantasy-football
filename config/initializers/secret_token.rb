31e75dccf5e364084b0dbaff1ed3aa457587e649
Exeggcute <imran.pshakir@hotmail.com>
2014-01-09 21:22:26 +0000

    autogenerated rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Footbawwl::Application.config.secret_key_base = '5d8597d2a9f2f6430d09f304f4a792db4974f7859f9e0393d3b7cb63dc6d4c2440d432eec6c1d2e7ce0399f471f0cd0632bd101c0d899cbcf3ae92b31d0f9857'
