cb7a943dadd30e94d3cc600ded7565a3c1b0d41c
Mike Spitz <ms1508@my.bristol.ac.uk>
2014-02-19 20:56:00 +0000

    #1-18 expanded on tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -10,6 +10,7 @@ class ActiveSupport::TestCase
   # Note: You'll currently still have to declare fixtures explicitly in integration tests
   # -- they do not yet inherit this setting
   fixtures :all
+  self.use_instantiated_fixtures = true
 
   # Add more helper methods to be used by all tests here...
 end
