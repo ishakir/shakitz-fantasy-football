01f7ef12a6d4edc9827a5346d1b5815de76b3764
Mike Spitz <ms1508@my.bristol.ac.uk>
2014-02-22 13:18:32 +0000

    Added user controller and tests, as well as updating test helper and routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Footbawwl::Application.routes.draw do
+  get "user/viewall"
+  get "user/show/:id" => "user#show", as: :showuser
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
