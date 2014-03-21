374b03220e593fe2ca1762f803be7df56bf4ef7a
Mike <timmysharwood@gmail.com>
2014-03-21 23:36:49 +0000

    Fixed nfl_player_test to put expected before actual, added match_player schema, plus tests for default value. Created match player controller with few default actions

--- db/schema.rb
+++ db/schema.rb
@@ -14,8 +14,24 @@
 ActiveRecord::Schema.define(version: 20140303213245) do
 
   create_table "match_players", force: true do |t|
-    t.integer  "touchdown"
     t.integer  "nfl_player_id"
+    t.integer  "passing_yards",    default: 0
+    t.integer  "passing_td",       default: 0
+    t.integer  "rushing_yards",    default: 0
+    t.integer  "rushing_td",       default: 0
+    t.integer  "point_conversion", default: 0
+    t.integer  "offensive_sack",   default: 0
+    t.integer  "offensive_safety", default: 0
+    t.integer  "fumble",           default: 0
+    t.integer  "qb_pick",          default: 0
+    t.integer  "defensive_sack",   default: 0
+    t.integer  "defensive_td",     default: 0
+    t.integer  "defensive_safety", default: 0
+    t.integer  "turnover",         default: 0
+    t.integer  "defensive_yards",  default: 0
+    t.integer  "defensive_points", default: 0
+    t.integer  "kicker_points",    default: 0
+    t.integer  "blocked_kicks",    default: 0
     t.datetime "created_at"
     t.datetime "updated_at"
   end
---
+++ test/helpers/match_player_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class MatchPlayerHelperTest < ActionView::TestCase
+end
