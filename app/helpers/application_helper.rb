6d4070e445691c9abb3c0fdcef7b5f0f77551247
Mike Spitz <ms1508@my.bristol.ac.uk>
2014-05-05 22:29:22 +0100

    Come on Mike... I forgot to run the tests and validate again. I'm a bad person. I'm a bad person. I'm a bad person. I'm a bad person. I'm a bad person. I'm a bad person. I'm a bad person. I'm a bad person. I'm a bad person. I'm a bad person. I've also fixed the tests and sorted out naming conventions and trailing whitespace etc.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,9 +3,9 @@ module ApplicationHelper
   # Used for when data is in a squiffy state
   class IllegalStateError < StandardError
   end
-  
-  def is_active?(link_path)
-    if(params[:action] == 'show' && (link_path.include? "user"))
+
+  def active?(link_path)
+    if params[:controller] == 'user' && params[:action] == 'show' && (link_path.include? "user")
       link_path = "/user/#{controller.show.id}"
     end
     current_page?(link_path) ? "active" : ""
