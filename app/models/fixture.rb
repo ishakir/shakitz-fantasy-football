2db9b41df75eee010ac20d2261fe88862e2081d2
ishakir <shakimra@uk.ibm.com>
2014-08-28 18:42:31 +0100

    Additional logging to identify fixtures bug

--- app/models/fixture.rb
+++ app/models/fixture.rb
@@ -58,7 +58,12 @@ class Fixture < ActiveRecord::Base
   end
 
   def drawn?
-    home_team.points == away_team.points
+    Rails.logger.info "Checking if fixture between #{home_team.user.name} and #{away_team.user.name} was drawn"
+    home_points = home_team.points
+    away_points = away_team.points
+    Rails.logger.info "#{home_team.user.name} points: #{home_points}"
+    Rails.logger.info "#{away_team.user.name} points: #{away_points}"
+    home_points == away_points
   end
 
   def lost_by?(team)
