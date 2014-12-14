19f20c6c383fbab0dfff2c45888042e37adf354a
ishakir <shakimra@uk.ibm.com>
2014-12-14 14:36:24 +0000

    Final rubocop fix ?

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,11 +20,7 @@ class User < ActiveRecord::Base
 
   def self.authenticate(name, password)
     user = find_by_name(name)
-    if user && user.password_hash == BCrypt::Engine.hash_secret(password, user.password_salt)
-      user
-    else
-      nil
-    end
+    return user if user && user.password_hash == BCrypt::Engine.hash_secret(password, user.password_salt)
   end
 
   def encrypt_password
