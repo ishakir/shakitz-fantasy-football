9e36add9e0e5c8116366f3fcde7cdbffd1f5b99b
ishakir <shakimra@uk.ibm.com>
2014-12-28 09:50:29 +0000

    Fix to user page crashing when logged in

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -174,8 +174,8 @@ class UserController < ApplicationController
     user_id = session[:user_id] if user_id <= 0
     team = User.find(user_id).team_for_current_game_week.match_players
 
-    team.reduce([]) do |player, array|
-      array.push([player.nfl_player_id, player.nfl_player.name, player.nfl_player.nfl_team.name])
+    team.map do |player|
+      [player.nfl_player_id, player.nfl_player.name, player.nfl_player.nfl_team.name]
     end
   end
   helper_method :return_player_name_for_active_game_week_team
