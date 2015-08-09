d836e3854658a59ad812a8546babdffd7ea5da34
ishakir <imran.pshakir@gmail.com>
2015-08-09 19:54:13 +0100

    Fix for ruby 2.2.0 breaking Time.zone.parse

--- app/models/with_game_week.rb
+++ app/models/with_game_week.rb
@@ -37,7 +37,7 @@ module WithGameWeek
   end
 
   def self.start_of_first_gameweek
-    Time.zone.parse(Settings.first_gameweek_start)
+    Time.parse(Settings.first_gameweek_start).localtime
   end
 
   def self.more_than_days_since_start?(days)
