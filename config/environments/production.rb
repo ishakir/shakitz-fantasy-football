5009222a513db810ad5d7408dae1fba67551bd08
Mike Spitz <ms1508@my.bristol.ac.uk>
2015-08-22 22:19:07 +0100

    Fix issues in production config file with regards to assets being precompiled. Hopefully fix refresh issue

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,7 +25,7 @@ Footbawwl::Application.configure do
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
-  # config.assets.css_compressor = :sass
+  config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
@@ -60,7 +60,7 @@ Footbawwl::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  config.assets.precompile += %w( .svg .eot .woff .ttf home.js team_show.js )
+  #config.assets.precompile += %w( .svg .eot .woff .woff2 .ttf )
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
