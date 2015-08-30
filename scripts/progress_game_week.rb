50cb8a2c6d0d7baedb0f291958b11dd19fb76866
ishakir <imran.pshakir@gmail.com>
2015-08-30 13:23:50 +0100

    Fix incorrect method in progress_game_week script

--- scripts/progress_game_week.rb
+++ scripts/progress_game_week.rb
@@ -2,7 +2,7 @@ require 'game_week_progresser'
 
 current_game_week = WithGameWeek.current_game_week
 next_game_week = current_game_week + 1
-have_already_progressed = GameWeekTeamPlayer.all.filter do |gwtp|
+have_already_progressed = GameWeekTeamPlayer.all.select do |gwtp|
   gwtp.match_player.game_week.number == next_game_week && gwtp.game_week_team.game_week.number == next_game_week
 end.empty?
 
