87c1807c714e6179fae065a7d81c45b6e48684a7
Mike Spitz <ms1508@my.bristol.ac.uk>
2015-10-01 10:45:19 +0100

    No longer assume there won't be one unique waiver per nfl player

--- app/models/waiver_wire.rb
+++ app/models/waiver_wire.rb
@@ -30,16 +30,17 @@ class WaiverWire < ActiveRecord::Base
 
     transferred_list = [] # keep memory of who has been added
     waiver.each do |w|
-      player_out_match = MatchPlayer.find_by nfl_player_id: w['player_out_id'].to_i
-      player_in_match = MatchPlayer.find_by nfl_player_id: w['player_in_id'].to_i
+      player_out_match = MatchPlayer.find_by nfl_player_id: w['player_out_id'].to_i, game_week_id: w['game_week_id'].to_i
+      player_in_match = MatchPlayer.find_by nfl_player_id: w['player_in_id'].to_i, game_week_id: w['game_week_id'].to_i
       # Don't re-add players, or execute if incoming player doesn't exist.
       if transferred_list.include?(w['player_in_id'].to_i) ||
          transferred_list.include?(w['player_out_id'].to_i) || player_in_match.nil?
         WaiverWire.find(w['id']).destroy # Waiver won't get executed, lets destroy it
         next
       end
-
+      Rails.logger.info "Swapping out #{player_out_match.id} for #{player_in_match.id}"
       player_out = player_out_match.game_week_team_players
+      Rails.logger.info "Outgoing player has gwtp: #{player_out}"
       player_team = User.find(w['user_id']).team_for_current_game_week
       is_playing = player_out[0].playing?
 
