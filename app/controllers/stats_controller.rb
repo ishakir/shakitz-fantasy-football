003b4e559379f16974ba0cebd12e3dadd2a59dff
Mike Spitz <ms1508@my.bristol.ac.uk>
2015-10-31 21:23:14 +0000

    Fix.. 480 rubocop errors cause mister *imran* didn't run rubocop before pushing

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,61 +1,61 @@
 class StatsController < ApplicationController
-	def show
-		current_game_week = WithGameWeek.current_game_week
-		current_game_week_with_some_stats = WithGameWeek.current_unlocked_game_week - 1
+  def show
+    current_game_week = WithGameWeek.current_game_week
+    current_game_week_with_some_stats = WithGameWeek.current_unlocked_game_week - 1
 
-		all_game_week_teams = User.all.map do |user|
-			(1 .. current_game_week).map do |game_week|
-				user.team_for_game_week(game_week)
-			end
-		end
+    all_game_week_teams = User.all.map do |user|
+      (1..current_game_week).map do |game_week|
+        user.team_for_game_week(game_week)
+      end
+    end
 
-		without_current_week = all_game_week_teams.map do |list|
-			list[0...-1]
-		end
+    without_current_week = all_game_week_teams.map do |list|
+      list[0...-1]
+    end
 
-		@bench_greater_than_team = without_current_week.flatten.select do |game_week_team|
-			game_week_team.bench_points > game_week_team.points
-		end
+    @bench_greater_than_team = without_current_week.flatten.select do |game_week_team|
+      game_week_team.bench_points > game_week_team.points
+    end
 
-		@bench_greater_than_one_hundred = without_current_week.flatten.select do |game_week_team|
-			game_week_team.bench_points > 100
-		end
+    @bench_greater_than_one_hundred = without_current_week.flatten.select do |game_week_team|
+      game_week_team.bench_points > 100
+    end
 
-		rows_hash = Hash.new
-		all_game_week_teams.each do |game_week_teams|
-			rows_hash[game_week_teams[0].user] = {
-				total_bench_points: game_week_teams.map{ |game_week_team| game_week_team.bench_points }.sum,
-				wins: 0
-			}
-		end
+    rows_hash = {}
+    all_game_week_teams.each do |game_week_teams|
+      rows_hash[game_week_teams[0].user] = {
+        total_bench_points: game_week_teams.map(&:bench_points).sum,
+        wins: 0
+      }
+    end
 
-		(1 ... current_game_week).each do |game_week|
-			user = User.all.map{|user| user.team_for_game_week(game_week)}.max_by{|game_week_team| game_week_team.bench_points}.user
-			rows_hash[user][:wins] = rows_hash[user][:wins] + 1
-		end
+    (1...current_game_week).each do |game_week|
+      user = User.all.map { |user| user.team_for_game_week(game_week) }.max_by(&:bench_points).user
+      rows_hash[user][:wins] = rows_hash[user][:wins] + 1
+    end
 
-		@rows = rows_hash.map do |user, hash|
-			hash["user"] = user
-			hash
-		end.sort_by do |hash|
-			hash["total_bench_points"]
-		end
+    @rows = rows_hash.map do |user, hash|
+      hash['user'] = user
+      hash
+    end.sort_by do |hash|
+      hash['total_bench_points']
+    end
 
-		@points = Hash[
-			User.all.map do |user| 
-				sum = 0
-				[
-					user.name, 
-					(1 .. current_game_week_with_some_stats).map do |game_week|
-						user.team_for_game_week(game_week).points 
-					end.map do |points|
-						sum += points
-					end.each_with_index.map do |cumulative_points, index|
-						# index + 1 is the number of gameweeks up till now
-						cumulative_points / (index + 1)
-					end
-				]
-			end
-		]
-	end
+    @points = Hash[
+      User.all.map do |user|
+        sum = 0
+        [
+          user.name,
+          (1..current_game_week_with_some_stats).map do |game_week|
+            user.team_for_game_week(game_week).points
+          end.map do |points|
+            sum += points
+          end.each_with_index.map do |cumulative_points, index|
+            # index + 1 is the number of gameweeks up till now
+            cumulative_points / (index + 1)
+          end
+        ]
+      end
+    ]
+  end
 end
