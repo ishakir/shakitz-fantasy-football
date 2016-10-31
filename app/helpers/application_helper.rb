b581d1827060244d32515b688ccc0b6288d13ce0
Mike Spitz <ms1508@my.bristol.ac.uk>
2016-10-31 00:52:02 +0000

    Fix the active class not being applied to the navigation for game days, stats, and the smackboard

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,9 +5,13 @@ module ApplicationHelper
   end
 
   def active?(link_path)
-    if params[:controller] == 'user' && params[:action] == 'show' && (link_path.include? 'user')
+    #Handle current_page logic differently based on whether it is a 'normal' route or game_days route (which causes urlexceptions otherwise)
+    if params[:controller] == 'user' && params[:action] == 'show' && (link_path.include? 'user') || params[:controller] == 'game_day'
       link_path = "/user/#{params[:user_id]}"
+    elsif link_path == '/game_day' and request.original_url.include? '/game_day'
+      'active'
+    else
+      current_page?(link_path) ? 'active' : ''
     end
-    current_page?(link_path) ? 'active' : ''
   end
 end
